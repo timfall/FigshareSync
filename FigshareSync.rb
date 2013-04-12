c3bce5f8004970ca967f2d29af3c2856ff29aff8
OpenShift guest <timfall@gmail.com>
2013-04-12 15:42:52 -0400

    initial article grab and debug print

--- FigshareSync.rb
+++ FigshareSync.rb
@@ -1,8 +1,11 @@
 require 'oath'
 require 'json'
 
-consumer = OAuth::Consumer.new('123456','65xyAzi1',{:site=>"http://api.figshare.com"})
+consumer = OAuth::Consumer.new('key','secret',{:site=>"http://api.figshare.com"})
 token = { :oauth_token => 'Ep6EHnBqnazUeeKfTQn9HwrC7mvfwynQHr00mAZo07iwEp6EHnXqnazUeeKfTQn9Hw',
           :oauth_token_secret => 'soPdczGixUt3JKEKw51AQA'
         }
 client = OAuth::AccessToken.from_hash(consumer, token)
+
+articlelist = client.get('/v1/my_data/articles')
+print articlelist.body
