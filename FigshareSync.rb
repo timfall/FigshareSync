bbede582c95cbf41e3b43fbcfc4d798c18e54ba7
OpenShift guest <timfall@gmail.com>
2013-04-15 01:25:01 -0400

    add yield

--- FigshareSync.rb
+++ FigshareSync.rb
@@ -3,8 +3,6 @@
 require 'oauth'
 require 'json'
 
-$pathtoarticledirectory
-
 consumer = OAuth::Consumer.new('NXueJD2t1DMXN7cmBgmcnA','aNZEUQdeYpXWSsu0INnw7A',{:site=>"http://api.figshare.com"})
 token = { :oauth_token => 'Ep6EHnBqnazUeeKfTQn9HwrC7mvfwynQHr00mAZo07iwEp6EHnXqnazUeeKfTQn9Hw',
           :oauth_token_secret => 'soPdczGixUt3JKEKw51AQA'
@@ -26,5 +24,6 @@ i.times do#i should be replaced with parsed['count']
     |currentfile|
     inputfile = File.open ('file.example')#file.example should be replaced with local file
     filehash = OpenSSL::Digest.new('sha256', 'inputfile')
+    yield
     puts "#{inputfile.path} has #{filehash.name} hash of #{filehash}"#debug
 end
