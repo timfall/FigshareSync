6b2ffa0a0a31647a7508565aa1f4a2afe6474cbb
OpenShift guest <timfall@gmail.com>
2013-04-15 01:19:55 -0400

    initial commit of classes

--- FigshareSync.rb
+++ FigshareSync.rb
@@ -3,6 +3,8 @@
 require 'oauth'
 require 'json'
 
+$pathtoarticledirectory
+
 consumer = OAuth::Consumer.new('NXueJD2t1DMXN7cmBgmcnA','aNZEUQdeYpXWSsu0INnw7A',{:site=>"http://api.figshare.com"})
 token = { :oauth_token => 'Ep6EHnBqnazUeeKfTQn9HwrC7mvfwynQHr00mAZo07iwEp6EHnXqnazUeeKfTQn9Hw',
           :oauth_token_secret => 'soPdczGixUt3JKEKw51AQA'
@@ -24,5 +26,5 @@ i.times do#i should be replaced with parsed['count']
     |currentfile|
     inputfile = File.open ('file.example')#file.example should be replaced with local file
     filehash = OpenSSL::Digest.new('sha256', 'inputfile')
-    puts "#{inputfile} has #{filehash.name} hash of #{filehash}"#debug
+    puts "#{inputfile.path} has #{filehash.name} hash of #{filehash}"#debug
 end
