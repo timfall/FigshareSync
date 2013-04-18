96a20e176e38961a88851914109b33a5df9724ee
Tim Fall <timfall@gmail.com>
2013-04-18 18:27:30 -0400

    Add local username to git

--- lib/FigshareClasses.rb
+++ lib/FigshareClasses.rb
@@ -1,7 +1,18 @@
 #!/usr/bin/ruby
 
-class article
-    def initialize (name, localpath)
+class Article
+    def initialize (name, localpath, id, title, description, defined_type)
         @name = name
         @path = localpath
+        @id = id
+        @title = title
+        @description = description
+        @defined_type = defined_type
+    end
+    
+    def hash
+        inputfile = File.open ('path')
+        filehash = OpenSSL::Digest.new('sha256', 'inputfile')
+        puts "#{inputfile.path} has #{filehash.name} hash of #{filehash}"#debug
+        return filehash
     end
