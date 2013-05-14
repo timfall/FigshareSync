1c3f1f375af905241f6a13cb9312d931661de85a
Tim Fall <timfall@gmail.com>
2013-05-14 13:45:36 -0400

    Made ArticleDatabase path entry depend on remote or local type

--- lib/FigshareClasses.rb
+++ lib/FigshareClasses.rb
@@ -40,11 +40,16 @@ end
     
 class ArticleDatabase
     def initialize (authtoken, {type, localdbfile})
-        if type == "remote" {db = authtoken.get ('/v1/my_data/articles')}
-        elsif type == "local" {db.body = localdbfile}
+        @path = nil
+        if type == "remote"
+            db = authtoken.get ('/v1/my_data/articles')
+            @path = '/v1/my_data/articles'
+        elsif type == "local"
+            db.body = localdbfile
+            @path = "#{localpath}/localdb/"
         db = JSON.parse (db.body)
         db[:item].each_object do |article_id|
-            articledatabase[article_id] = Article.new(db[:item]['name'], '#{localpath}/localdb/#{article_id}', db[:item]['article_id'], db[:item]['title'], db[:item]['description'], db[:item]['defined_type'])#create new article entry for each article listed
+            articledatabase[article_id] = Article.new(db[:item]['name'], "#{@path}/#{db[:item]['article_id']}", db[:item]['article_id'], db[:item]['title'], db[:item]['description'], db[:item]['defined_type'])#create new article entry for each article listed
             aritcledatabase[article_id].populate(authtoken)
         end
         return articledatabase
